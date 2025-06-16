c4915904184ac1b996e393bc0d715d5debe6ce96
Inácio Oliveira <inacioigne@gmail.com>
2025-06-16 13:37:07 -0400

    16/06/2025

--- ui/src/themes/eager-themes.module.ts
+++ ui/src/themes/eager-themes.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+// import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+import { EagerThemeModule as InpaEagerThemeModule } from './inpa/eager-theme.module';
 
 /**
  * This module bundles the eager theme modules for all available themes.
@@ -12,7 +13,8 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
+    InpaEagerThemeModule,
+    // DSpaceEagerThemeModule,
     // CustomEagerThemeModule,
   ],
 })
