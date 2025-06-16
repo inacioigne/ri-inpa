4d5d91b6b86311124dd060390f625edf5d396391
Inácio Oliveira <inacioigne@gmail.com>
2025-06-16 16:51:40 -0400

    16/06/2025

---
+++ ui/src/themes/inpa_bha_col/lazy-theme.module.ts
@@ -0,0 +1,57 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+import { RootModule } from '../../app/root.module';
+import { CommunityPageSubCommunityListComponent } from './app/community-page/sections/sub-com-col-section/sub-community-list/community-page-sub-community-list.component';
+import { CommunityPageSubCollectionListComponent } from './app/community-page/sections/sub-com-col-section/sub-collection-list/community-page-sub-collection-list.component';
+import { CommunityPageComponent } from './app/community-page/community-page.component';
+
+
+
+const DECLARATIONS = [
+  CommunityPageSubCommunityListComponent,
+  CommunityPageSubCollectionListComponent,
+  CommunityPageComponent
+];
+
+@NgModule({
+  imports: [
+    RootModule,
+    CommonModule,
+    DragDropModule,
+    FormsModule,
+    NgbModule,
+    RouterModule,
+    ScrollToModule,
+    StoreModule,
+    StoreRouterConnectingModule,
+    TranslateModule,
+    FormsModule,
+    ...DECLARATIONS
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
+
+/**
+ * This module serves as an index for all the components in this theme.
+ * It should import all other modules, so the compiler knows where to find any components referenced
+ * from a component in this theme
+ * It is purposefully not exported, it should never be imported anywhere else, its only purpose is
+ * to give lazily loaded components a context in which they can be compiled successfully
+ */
+class LazyThemeModule {
+}
