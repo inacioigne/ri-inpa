107c3cd08a7642378d8afa3ffb1f5cf3a46697c0
Inácio Oliveira <inacioigne@gmail.com>
2026-07-03 16:54:15 -0400

    03/07/2026

--- script/item_update.sh
+++ script/item_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-CONTAINER="api"
-DIR="/dspace/item"
+HANDLE=$1
 
 # Verifica se o diretório existe
 if docker exec "api" test -d "/dspace/item"; then
@@ -15,7 +14,7 @@ else
 fi
 
 echo "Exportando item..."
-if docker exec "api" bash -c "/dspace/bin/dspace export -t ITEM -d /dspace/item -n 1 -i inpa/128"; then
+if docker exec "api" bash -c "/dspace/bin/dspace export -t ITEM -d /dspace/item -n 1 -i $HANDLE"; then
     echo "OK: Item exportado com sucesso."
 else
     echo "ERRO: Não foi possível exportar o item."
