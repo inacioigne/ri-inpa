eaaf9e8378b1a93af32d655cc0eb2d16d96dbf67
Inácio Oliveira <inacioigne@gmail.com>
2026-09-21 10:29:30 -0400

    21/09/26

--- ui/src/themes/inpa/app/inpa-component/date-metadata/date-metadata.component.ts
+++ ui/src/themes/inpa/app/inpa-component/date-metadata/date-metadata.component.ts
@@ -1,16 +1,20 @@
+import { AsyncPipe, JsonPipe } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { Item } from '@dspace/core/shared/item.model';
-import { ItemPageDateFieldComponent } from "src/app/item-page/simple/field-components/specific-field/date/item-page-date-field.component";
+// import { ItemPageDateFieldComponent } from "src/app/item-page/simple/field-components/specific-field/date/item-page-date-field.component";
+import { ItemPageFieldComponent } from "src/app/item-page/simple/field-components/specific-field/item-page-field.component";
 
 @Component({
     selector: 'inpa-date-metadata',
     templateUrl: './date-metadata.component.html',
     styleUrls: ['./date-metadata.component.scss'],
     imports: [
-        ItemPageDateFieldComponent
+        AsyncPipe,
+        JsonPipe
+        // ItemPageDateFieldComponent
     ],
 })
-export class DateMetadataComponent {
+export class DateMetadataComponent extends ItemPageFieldComponent  {
 
     @Input() item: Item;
 
