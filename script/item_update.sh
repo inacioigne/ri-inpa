dc7c56ab483cf4967752aee225fc111acedae337
Inácio Oliveira <inacioigne@gmail.com>
2026-09-21 21:24:38 -0400

    21/09/26

--- script/item_update.sh
+++ script/item_update.sh
@@ -4,27 +4,27 @@ HANDLE=$1
 PDF=$2
 
 # Verifica se o diretório existe
-if docker exec "api" test -d "/dspace/item"; then
-    if docker exec "api" test -d "/dspace/item/1"; then
-        docker exec "api" rm -rf "/dspace/item/1";
-        echo "OK: Diretório '/dspace/item/1' removido com sucesso."
+if docker exec "api" test -d "/dspace/export/item"; then
+    if docker exec "api" test -d "/dspace/export/item/1"; then
+        docker exec "api" rm -rf "/dspace/export/item/1";
+        echo "OK: Diretório removido com sucesso."
     fi
 else 
-    echo "Criando diretório '/dspace/item'..."
-    docker exec "api" mkdir -p "/dspace/item"
+    echo "Criando diretório ..."
+    docker exec "api" mkdir -p "/dspace/export/item"
 fi
 
 echo "Exportando item $HANDLE..."
-if docker exec "api" bash -c "/dspace/bin/export export -t ITEM -d /dspace/item -n 1 -i $HANDLE"; then
+if docker exec "api" bash -c "/dspace/bin/dspace export -t ITEM -d /dspace/export/item -n 1 -i $HANDLE"; then
     echo "OK: Item exportado com sucesso."
     echo "Removendo bitstream ORIGINAL..."
-    docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s ./item -D ORIGINAL"
-    docker cp /home/${PDF} api:/dspace/item/1/
+    docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/export/item -D ORIGINAL"
+    docker cp /home/${PDF} api:/dspace/export/item/1/
     sed -i "s|^[^[:space:]]*|$PDF|" /home/contents
-    docker cp /home/contents api:/dspace/item/1/
+    docker cp /home/contents api:/dspace/export/item/1/
     echo "Arquivos copiados com sucesso."
     echo "Atualizando item $HANDLE..."
-    if docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/item -A"; then
+    if docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/export/item -A"; then
         echo "OK: Item atualizado com sucesso."
         echo "Indexando item $HANDLE..."
         docker exec "api" bash -c "./bin/dspace filter-media -i $HANDLE"
