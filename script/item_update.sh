a75e0f3c12a3fba6e508e6a146a7a9eaf059ac52
Inácio Oliveira <inacioigne@gmail.com>
2026-09-21 21:39:20 -0400

    21/09/26

--- script/item_update.sh
+++ script/item_update.sh
@@ -4,27 +4,27 @@ HANDLE=$1
 PDF=$2
 
 # Verifica se o diretório existe
-if docker exec "api" test -d "/dspace/export"; then
-    if docker exec "api" test -d "/dspace/export/1"; then
-        docker exec "api" rm -rf "/dspace/export/1";
+if docker exec "api" test -d "/dspace/exports"; then
+    if docker exec "api" test -d "/dspace/exports/1"; then
+        docker exec "api" rm -rf "/dspace/exports/1";
         echo "OK: Diretório removido com sucesso."
     fi
 else 
     echo "Criando diretório ..."
-    docker exec "api" mkdir -p "/dspace/export"
+    docker exec "api" mkdir -p "/dspace/exports"
 fi
 
 echo "Exportando item $HANDLE..."
-if docker exec "api" bash -c "/dspace/bin/dspace export -t ITEM -d /dspace/export/ -n 1 -i $HANDLE"; then
+if docker exec "api" bash -c "/dspace/bin/dspace export -t ITEM -d /dspace/exports/ -n 1 -i $HANDLE"; then
     echo "OK: Item exportado com sucesso."
     echo "Removendo bitstream ORIGINAL..."
-    docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/export/ -D ORIGINAL"
-    docker cp /home/${PDF} api:/dspace/export/1/
+    docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/exports/ -D ORIGINAL"
+    docker cp /home/${PDF} api:/dspace/exports/1/
     sed -i "s|^[^[:space:]]*|$PDF|" /home/contents
-    docker cp /home/contents api:/dspace/export/1/
+    docker cp /home/contents api:/dspace/exports/1/
     echo "Arquivos copiados com sucesso."
     echo "Atualizando item $HANDLE..."
-    if docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/export/ -A"; then
+    if docker exec "api" bash -c "./bin/dspace itemupdate -e ri@inpa.gov.br -s /dspace/exports/ -A"; then
         echo "OK: Item atualizado com sucesso."
         echo "Indexando item $HANDLE..."
         docker exec "api" bash -c "./bin/dspace filter-media -i $HANDLE"
